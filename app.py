4ff588044b66df57ae387f5af139f152fcaa358e
MuhammadHasib1204 <muhammadhasib02@gmail.com>
2026-06-28 11:20:23 +0700

    First commit

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-import os
 from flask import Flask
 from routes import init_routes
+import os
 
 app = Flask(__name__)
-app.secret_key = 'skripsi_hnbakery_super_rahasia'
+app.secret_key = os.getenv("SECRET_KEY", "skripsi_hnbakery")
 
 init_routes(app)
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000)),
-        debug=False
-    )
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
--- models.py
+++ models.py
@@ -5,12 +5,13 @@ def get_db_connection():
     try:
         conn = mysql.connector.connect(
             host=os.getenv("MYSQLHOST"),
-            port=int(os.getenv("MYSQLPORT")),
             user=os.getenv("MYSQLUSER"),
             password=os.getenv("MYSQLPASSWORD"),
-            database=os.getenv("MYSQLDATABASE")
+            database=os.getenv("MYSQLDATABASE"),
+            port=int(os.getenv("MYSQLPORT"))
         )
         return conn
+
     except mysql.connector.Error as err:
-        print(f"Error Koneksi: {err}")
+        print(err)
         return None
